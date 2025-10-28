a91d7a8c0e58770a0d6a46d549b90cbd42b20473
Minaazs <mum@discente.ifpe.edu.br>
2025-10-28 11:21:30 -0300

    build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,44 @@
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
-    id("androidx.navigation.safeargs.kotlin") version "2.7.7" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("androidx.navigation.safeargs.kotlin")
+}
+
+android {
+    namespace = "com.example.livrostore"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.livrostore"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    
+    val navVersion = "2.7.7"
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
+    
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
 }
